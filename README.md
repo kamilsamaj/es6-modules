b8bb7ad0da6e98affc8cc10b0c937fcb4aaccf02
kamilsamaj <kamilsamaj@gmail.com>
2021-03-16 07:54:22 +0100

    use es6 modules

---
+++ README.md
@@ -0,0 +1,3 @@
+# ES6 Modules
+
+[webpack-js](https://github.com/kamilsamaj/webpack-js) reworked to standard ES6 module system
---
+++ dom.js
@@ -0,0 +1,24 @@
+import getUsers from './users';
+
+function addUserToDOM(name) {
+    const node = document.createElement("li");
+    const text = document.createTextNode(name);
+    node.appendChild(text);
+
+    document.getElementById("users")
+        .appendChild(node);
+}
+
+document.getElementById("submit")
+    .addEventListener("click", () => {
+        const input = document.getElementById("input");
+        addUserToDOM(input.value);
+
+        input.value = "";
+    });
+
+let users = getUsers();
+
+for (let i = 0; i < users.length; i++) {
+    addUserToDOM(users[i]);
+}
